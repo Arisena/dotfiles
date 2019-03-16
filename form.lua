bb4e40b8e70e34975bd2538660b55334456921ca
Brendan McCourt <bcmccourt202@gmail.com>
2019-03-16 04:28:53 -0500

    Update form.lua

--- form.lua
+++ form.lua
@@ -1,3 +1,5 @@
+--Form : Version 2 : Created by Ari
+
 name = "" ; problem = "" ; note = ""
 
 --{{{Question Asking Part
@@ -12,6 +14,7 @@ note = io.read()
 --}}}
 
 --{{{File Writing Part
+--this part writes all the variable to a file
 file = io.open(os.date("%a%b%d,%H%M%S"), "w")
 file:write("Name: ", name, "\n")
 file:write("Problem: ", problem, "\n")
